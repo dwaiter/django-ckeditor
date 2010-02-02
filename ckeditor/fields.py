fbaa02b65b28d28b42fd4421fd7346d41bf84770
Steve Losh <steve@dwaiter.com>
2010-02-02 09:43:19 -0500

    Initial commit.
    The widget and field are functional but untested, and still need a bunch of
    improvements to be useful in real life.

---
+++ ckeditor/fields.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.contrib.admin import widgets as admin_widgets
+from ckeditor import widgets as ckeditor_widgets
+
+
+class HTMLField(models.TextField):
+    """A string field for HTML content. It uses the CKEditor widget in forms."""
+    
+    def formfield(self, **kwargs):
+        defaults = { 'widget': ckeditor_widgets.CKEditor }
+        defaults.update(kwargs)
+        
+        if defaults['widget'] == admin_widgets.AdminTextareaWidget:
+            defaults['widget'] = ckeditor_widgets.AdminCKEditor
+        
+        return super(HTMLField, self).formfield(**defaults)
+    
+
---
+++ ckeditor/widgets.py
@@ -0,0 +1,22 @@
+from django import forms
+from django.conf import settings
+from django.contrib.admin import widgets as admin_widgets
+
+
+class CKEditor(forms.Textarea):
+    def __init__(self, *args, **kwargs):
+        attrs = kwargs['attrs'] if 'attrs' in kwargs else {}
+        attrs['class'] = 'django-ckeditor'
+        kwargs['attrs'] = attrs
+        super(CKEditor, self).__init__(*args, **kwargs)
+    
+    class Media:
+        js = (
+            settings.MEDIA_URL.rstrip('/') + '/ckeditor/ckeditor/ckeditor.js',
+            settings.MEDIA_URL.rstrip('/') + '/ckeditor/init.js',
+        )
+
+
+class AdminCKEditor(admin_widgets.AdminTextareaWidget, CKEditor):
+    pass
+
