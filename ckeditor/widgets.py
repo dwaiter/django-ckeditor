f834a933a08a2fc1a9cd8b66c3f55694c76d28e5
Steve Losh <steve@dwaiter.com>
2010-03-04 11:45:05 -0500

    Big refactoring to make lots of stuff better.
    Yeah, I know this is a bad commit message, sorry!

--- ckeditor/widgets.py
+++ ckeditor/widgets.py
@@ -1,6 +1,17 @@
+try:
+    import simplejson as json
+except ImportError:
+    import json
+
 from django import forms
 from django.conf import settings
 from django.contrib.admin import widgets as admin_widgets
+from django.template.loader import render_to_string
+from django.utils.safestring import mark_safe
+
+
+CKEDITOR_CONFIGS = dict((k, json.dumps(v)) for k, v in settings.CKEDITOR_CONFIGS.items())
+FILEBROWSER_PRESENT = 'filebrowser' in getattr(settings, 'INSTALLED_APPS', [])
 
 
 class CKEditor(forms.Textarea):
@@ -8,13 +19,35 @@ class CKEditor(forms.Textarea):
         attrs = kwargs['attrs'] if 'attrs' in kwargs else {}
         attrs['class'] = 'django-ckeditor'
         kwargs['attrs'] = attrs
+        
+        if 'ckeditor_config' in kwargs:
+            self.ckeditor_config = kwargs.pop('ckeditor_config')
+        else:
+            self.ckeditor_config = 'default'
+        
         super(CKEditor, self).__init__(*args, **kwargs)
+    
+    def render(self, name, value, attrs=None, **kwargs):
+        rendered = super(CKEditor, self).render(name, value, attrs)
+        context = {
+            'name': name,
+            'config': CKEDITOR_CONFIGS[self.ckeditor_config],
+            'filebrowser': FILEBROWSER_PRESENT,
+        }
+        return rendered +  mark_safe(render_to_string(
+            'ckeditor/ckeditor_script.html', context
+        ))
     
     class Media:
         js = (
             settings.MEDIA_URL.rstrip('/') + '/ckeditor/ckeditor/ckeditor.js',
-            '/ckeditor/init.js',
         )
+        css = {
+            'screen': (
+                settings.MEDIA_URL.rstrip('/') + '/ckeditor/css/grappelli.css',
+            ),
+        }
+    
 
 
 class AdminCKEditor(admin_widgets.AdminTextareaWidget, CKEditor):
