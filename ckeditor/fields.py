9fc93b325e19410cc33f95ae4a6e8d19667864c1
Mike Korobov <kmike84@gmail.com>
2010-04-10 19:43:09 +0600

    HTMLField is now compatible with South

--- ckeditor/fields.py
+++ ckeditor/fields.py
@@ -5,14 +5,18 @@ from ckeditor import widgets as ckeditor_widgets
 
 class HTMLField(models.TextField):
     """A string field for HTML content. It uses the CKEditor widget in forms."""
-    
+
     def formfield(self, **kwargs):
         defaults = { 'widget': ckeditor_widgets.CKEditor }
         defaults.update(kwargs)
-        
+
         if defaults['widget'] == admin_widgets.AdminTextareaWidget:
             defaults['widget'] = ckeditor_widgets.AdminCKEditor
-        
-        return super(HTMLField, self).formfield(**defaults)
-    
 
+        return super(HTMLField, self).formfield(**defaults)
+
+try:
+    from south.modelsinspector import add_introspection_rules
+    add_introspection_rules([], ["^ckeditor\.fields\.HTMLField"])
+except ImportError:
+    pass
