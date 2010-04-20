30513143208cfd58cc3adbea298a534778f8e3ef
Steve Losh <steve@stevelosh.com>
2010-04-20 12:40:48 -0400

    Update version number.

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ else:
 
 setup(
     name='django-ckeditor',
-    version='0.9.1',
+    version='0.9.2',
     install_requires=install_requires,
     description=description,
     long_description=long_description,
