43176672224dec8e222592f5452536fb17eac1e0
Steve Losh <steve@stevelosh.com>
2011-06-07 12:16:52 -0400

    Increment version.

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ else:
 
 setup(
     name='django-ckeditor',
-    version='0.9.3',
+    version='0.9.5',
     install_requires=install_requires,
     description=description,
     long_description=long_description,
